239c41ee0d618f6bac30293ca4ff665ff631f8e9
michelangelo17 <michelangelo.markus@gmail.com>
2020-07-26 23:55:32 -0700

    MVP + Stretch Goals Complete

--- src/animalSearch/Main.java
+++ src/animalSearch/Main.java
@@ -6,6 +6,20 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Main {
+
+  public static List<Animal> filteredAnimals = new ArrayList<>();
+  public static void filterAnimals(List<Animal> animals, AnimalFilter af) {
+    filteredAnimals.clear();
+    for (Animal a : animals) {
+      if (af.filter(a)) {
+        filteredAnimals.add(a);
+      }
+    }
+  }
+
+  public static void printFA() {
+    filteredAnimals.forEach(a -> System.out.println(a.getName() + " " + a.reproduce() + " " + a.move() + " " + a.breathe() + " " + a.getYearDisc()));
+  }
   
   public static void main(String[] args) {
     List<Animal> animals = new ArrayList<>();
@@ -40,5 +54,27 @@ public class Main {
     animals.sort((a1, a2) -> a1.move().compareToIgnoreCase(a2.move()));
     System.out.println("\n*** List all the animals order by how they move ***\n");
     System.out.println(animals);
+
+    System.out.println("\n*** List only those animals the breathe with lungs ***\n");
+    filterAnimals(animals, a -> a.breathe() == "lungs");
+    printFA();
+
+    System.out.println("\n*** List only those animals the breathe with lungs and were named in 1758 ***\n");
+    filterAnimals(animals, a -> (a.breathe() == "lungs") && (a.getYearDisc() == 1758));
+    printFA();
+
+    System.out.println("\n*** List only those animals that lay eggs and breath with lungs ***\n");
+    filterAnimals(animals, a -> (a.reproduce() == "eggs") && (a.breathe() == "lungs"));
+    printFA();
+
+    System.out.println("\n*** List alphabetically only those animals that were named in 1758 ***\n");
+    filterAnimals(animals, a -> a.getYearDisc() == 1758);
+    filteredAnimals.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+    printFA();
+
+    System.out.println("\n*** For the list of animals, list alphabetically those animals that are mammals ***\n");
+    filterAnimals(animals, a -> a instanceof Mammal);
+    filteredAnimals.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+    printFA();
   }
 }
